5e21731be1028a9753e6ff9a1db2f6bc3486064c
mberkelmans <50192211+mberkelmans@users.noreply.github.com>
2019-05-30 18:36:30 +1000

    Update myscript4.sh

--- myscript4.sh
+++ myscript4.sh
@@ -7,6 +7,7 @@ formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.f
 # starting at 50, then incrementing by 50 each iteration, stopping at 950
 for i in $(seq 50 50 950); do
 
+# printing the count of potential mismatch sites on the output file
 echo $i
 
 # loop through 100 iterations per nucelotide count 
