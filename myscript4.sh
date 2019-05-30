22e3759253cf2e584bb7d2e19d70a64819bd7e92
mberkelmans <50192211+mberkelmans@users.noreply.github.com>
2019-05-30 18:40:01 +1000

    Update myscript4.sh

--- myscript4.sh
+++ myscript4.sh
@@ -7,15 +7,15 @@ formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.f
 # starting at 50, then incrementing by 50 each iteration, stopping at 950
 for i in $(seq 50 50 950); do
 
-# printing the count of potential mismatch sites on the output file
+# print the count of potential mismatch sites on the output file
 echo $i
 
-# loop through 100 iterations per nucelotide count 
+# loop through 100 iterations per mismatch site count 
 for ((j=1; j<=100; j++)) ; do
-msbar -sequence mysequence.fa -count $i -point 4 -block 0 -codon 0 -outseq mysample_mutated2.fa
 
-blast2 -p blastn -e 0.001 -m 8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated2.fa \
-| grep "AAC76296" | head -1 | wc -l  
+# run  BLAST comparison
+msbar -sequence mysequence.fa -count $i -point 4 -block 0 -codon 0 -outseq mysample_mutated2.fa
+blast2 -p blastn -e 0.001 -m 8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated2.fa | grep "AAC76296" | head -1 | wc -l  
 
 done | numsum
 done  > output4.txt
