251369e215eb5d71e5938f885cfb462150107d72
mberkelmans <50192211+mberkelmans@users.noreply.github.com>
2019-05-30 18:44:30 +1000

    Update myscript4.sh

--- myscript4.sh
+++ myscript4.sh
@@ -14,9 +14,13 @@ echo $i
 for ((j=1; j<=100; j++)) ; do
 
 # run  BLAST comparison
+# count the number of times the accession number was found in the output (but with a maximum count of 1 per BLAST run)
 msbar -sequence mysequence.fa -count $i -point 4 -block 0 -codon 0 -outseq mysample_mutated2.fa
 blast2 -p blastn -e 0.001 -m 8 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated2.fa | grep "AAC76296" | head -1 | wc -l  
 
+# close loop 2 and add up the number of matches out of the 100 runs
 done | numsum
+
+# close loop 1
 done  > output4.txt
 
